284f260b1ac72feae368e46579c9e735e3ce5bab
Levi Reid <89493993+Superflyreid@users.noreply.github.com>
2021-11-29 11:19:18 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-(function () {
-  "use strict";
-
-  // Add Code Below Here
-
-})();
+function black() {
+  let color = "#111";
+  navigator.clipboard.writeText(color);
+}
+function white() {
+  let color = "#fff";
+  navigator.clipboard.writeText(color);
+}
+function darkgray() {
+  let color = "#343a40";
+  navigator.clipboard.writeText(color);
+}
+function gray() {
+  let color = "#6c757d";
+  navigator.clipboard.writeText(color);
+}
+function yellow() {
+  let color = "#ffc107";
+  navigator.clipboard.writeText(color);
+}
